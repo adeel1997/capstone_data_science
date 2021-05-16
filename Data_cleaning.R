670574cd657085559633cf3414671c5c14e1482c
Adeel Khan <adeel707@gmail.com>
2021-05-16 23:48:57 +0530

    Updating the data and adding country as a variable in the dataframe

--- Data_cleaning.R
+++ Data_cleaning.R
@@ -1,5 +1,5 @@
 library(stringr);library(dplyr)
-data <- read.delim("Data/earthquakes-2021-05-16_17-33-35_+0530.tsv")
+data <- read.delim("Data/earthquakes-2021-05-16_23-42-17_+0530.tsv")
 head(data)
 eq_clean_data <- function(data){
     ## Converting date into date column
@@ -10,18 +10,20 @@ eq_clean_data <- function(data){
     data$Longitude <- as.numeric(data$Longitude)
     ## Getting the location name and splitting with colon 
     location_name <- strsplit(data$Location.Name,split = ":")
+    ## Country is first in the list and second is region
+    Country <- sapply(location_name,"[",1)
+    ## Converting All caps to Title case
+    Country <- str_to_title(Country)
+    data$Country <- Country
     ## Removing the country and Selecting the region
-    location_name <- sapply(location_name,"[",2)
+    region <- sapply(location_name,"[",2)
     ## Removing extra white space
-    location_name <- str_trim(location_name)
+    region <- str_trim(region)
     ## Converting All caps to title case
-    location_name <- str_to_title(location_name)
+    region <- str_to_title(region)
     ## Converting into a vector
-    location_name <- as.vector(unlist(location_name))
-    data$Location_Name <- location_name
+    region <- as.vector(unlist(region))
+    data$Location_Name <- region
     return(data)
                             
 }
-
-clean_data <- eq_clean_data(data = data)
-head(clean_data)
